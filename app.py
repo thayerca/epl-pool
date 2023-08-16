53593428523c796595d699c3ab33ce254da1b37c
thayer6 <casey.thayer6@gmail.com>
2023-08-15 23:22:22 -0700

    initial setup

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template
+import pandas as pd
+
+app = Flask(__name__)
+
+owner_teams = {
+    "Arsenal": "Casey",
+    "Aston Villa": "Casey",
+    "Bournemouth": "Jason",
+    "Brentford": "CB",
+    "Brighton": "Megan",
+    "Burnley": "Neo",
+    "Chelsea": "CB",
+    "Crystal Palace": "Jason",
+    "Everton": "Megan",
+    "Fulham": "CB",
+    "Liverpool": "Neo",
+    "Luton Town": "CB",
+    "Manchester City": "Jason",
+    "Manchester United": "Megan",
+    "Newcastle Utd": "Neo",
+    "Nott'ham Forest": "Neo",
+    "Sheffield Utd": "Casey",
+    "Tottenham": "Jason",
+    "West Ham": "Casey",
+    "Wolves": "Megan",
+}
+
+owner_team_data = {
+    "Squad": [key for key in owner_teams.keys()],
+    "Owner": [val for val in owner_teams.values()]
+}
+
+owner_table = pd.DataFrame(data=owner_team_data)
+
+table_df = pd.read_html("https://fbref.com/en/comps/9/Premier-League-Stats")[0]
+merged_table = pd.merge(table_df, owner_table, on="Squad")
+final_table = merged_table.drop(
+    columns=[
+        "Last 5", "Attendance", "Top Team Scorer", "Goalkeeper", "Notes"
+    ]
+)
+
+@app.route('/', methods = ("POST", "GET"))
+def html_table():
+   return render_template(
+           'index.html',
+            tables=[final_table.to_html(
+            classes="table",
+            index=False,
+            header="true")]
+        )
---
+++ static/data/owner_teams_23_24.py
@@ -0,0 +1,8 @@
+owner_teams = {
+    "casey": ["Arsenal", "Aston Villa", "West Ham United", "Sheffield United"],
+    "megan": ["Manchester United", "Brighton and Hove Albion", "Wolverhampton Wanderers", "Everton"],
+    "neo": ["Liverpool", "Newcastle United", "Nottingham Forest", "Burnley"],
+    "jason": ["Manchester City", "Tottenham Hotspur", "Crystal Palace", "Bournemouth"],
+    "cb": ["Chelsea", "Brentford", "Fulham", "Luton Town"]
+    }
+
