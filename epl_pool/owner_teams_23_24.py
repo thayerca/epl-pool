9ef97bf5f70e2d134ecf1bb4f0f6489101d52837
thayer6 <casey.thayer6@gmail.com>
2024-08-21 15:54:39 -0700

    feat: add draft results dir

--- epl_pool/owner_teams_23_24.py
+++
@@ -1,8 +0,0 @@
-owner_teams = {
-    "casey": ["Arsenal", "Aston Villa", "West Ham United", "Sheffield United"],
-    "megan": ["Manchester United", "Brighton and Hove Albion", "Wolverhampton Wanderers", "Everton"],
-    "neo": ["Liverpool", "Newcastle United", "Nottingham Forest", "Burnley"],
-    "jason": ["Manchester City", "Tottenham Hotspur", "Crystal Palace", "Bournemouth"],
-    "cb": ["Chelsea", "Brentford", "Fulham", "Luton Town"]
-    }
-
